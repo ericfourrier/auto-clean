e9c3895d49ddf8efc80b2e75ddce5ee403e479b3
Eric Fourrier <ericfourrier@gmail.com>
2015-12-17 02:07:02 +0100

    Update Corrplot

--- autoc/utils/corrplot.py
+++ autoc/utils/corrplot.py
@@ -1,12 +1,15 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-def plot_corrmatrix(corr_matrix, square=True, linewidths=0.1, annot=True,
-                    size=None, *args, **kwargs):
+
+def plot_corrmatrix(df, square=True, linewidths=0.1, annot=True,
+                    size=None, figsize=(12, 9), *args, **kwargs):
     """
     Plot correlation matrix of the dataset
     see doc at https://stanford.edu/~mwaskom/software/seaborn/generated/seaborn.heatmap.html#seaborn.heatmap
 
     """
-    sns.heatmap(corr_matrix, vmin=corr_matrix.values.min(), vmax=1,
-                square=square, linewidths=linewidths, annot=annot,
-                annot_kws={"size": size}, *args, **kwargs)
+    sns.set(context="paper", font="monospace")
+    f, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(df.corr(), vmax=1, square=square, linewidths=linewidths,
+                annot=annot, annot_kws={"size": size}, *args, **kwargs)
