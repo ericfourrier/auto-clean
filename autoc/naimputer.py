db61af895fd175cf4682d381e5920765b793cc2e
Eric Fourrier <ericfourrier@gmail.com>
2016-01-02 23:53:16 +0100

    Update NaImputer adding corrplot_na , infos_na, isna_summary

--- autoc/naimputer.py
+++ autoc/naimputer.py
@@ -1,8 +1,9 @@
-from autoc.explorer import DataExploration
+from autoc.explorer import DataExploration, pd
 from autoc.utils.helpers import cserie
 import seaborn as sns
-import  matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt
+from autoc.utils.helpers import cached_property
+from autoc.utils.coorplot import plot_corrmatrix
 
 def missing_map(df, nmax=100, verbose=True, yticklabels=False, figsize=(15, 11), *args, **kwargs):
     """ Returns missing map plot like in amelia 2 package in R """
@@ -15,23 +16,58 @@ class NaImputer(DataExploration):
 
     def __init__(self, *args, **kwargs):
         super(NaImputer, self).__init__(*args, **kwargs)
-        self.data_na = pd.DataFrame # data with is_na_col
+
+    def infos_na(self, na_low=0.05, na_high=0.90):
+        """ Returns a dict with various infos about missing values """
+        infos = {}
+        infos['nacolcount'] = self.nacolcount()
+        infos['narowcount'] = self.narowcount()
+        infos['nb_total_na'] = self.total_missing
+        infos['many_na_col'] = self.manymissing(pct=na_high)
+        infos['low_na_col'] = self.nacolcount().Napercentage < na_low
+        infos['total_pct_na'] = self.nacolcount().Napercentage.mean()
+        return infos
 
     def get_isna(self, col):
+        """ Returns a dummy variable indicating in a observation of a specific col
+            is na or not 0 -> not na , 1 -> na """
         return self.data.loc[:, col].isnull().astype(int)
 
-    def get_isna_df(self, prefix="is_na_", subset=None):
+    @property
+    def data_isna_m(self):
+        """ Returns merged dataframe (data, data_is_na)"""
+        return pd.concat((self.data, self.data_isna), axis=1)
+
+    @cached_property
+    def data_isna(self, prefix="is_na_", subset=None):
         """ Returns dataset with is_na columns from the a dataframe with missing values """
         if subset is not None:
-            isna_df = self.data[subset].apply(lambda x: self.get_isna(x.name))
+            data_isna = self.data[subset].apply(lambda x: self.get_isna(x.name))
         else:
-            isna_df = self.data.apply(lambda x: self.get_isna(x.name))
-        isna_df.columns = ["{}{}".format(prefix, c) for c in isna_df.columns]
-        return pd.concat((self.data,isna_df), axis = 1)
+            data_isna = self.data.apply(lambda x: self.get_isna(x.name))
+        data_isna.columns = ["{}{}".format(prefix, c) for c in data_isna.columns]
+        return data_isna
 
-    def isna_summary(self, col):
+    def corrplot_na(self, remove_cst_col=True, *args, **kwargs):
+        """ Returns a corrplot of data_isna """
+        if remove_cst_col:
+            plot_corrmatrix(self.data_isna.loc[:, self.data_isna.var() > 0], *args, **kwargs)
+        else:
+            plot_corrmatrix(self.data_isna, *args, **kwargs)
+
+    def get_isna_mean(self, colname, prefix="is_na_"):
+        """ Returns empirical conditional expectatation, std, and sem of other numerical variable
+        for a certain colname with 0:not_a_na 1:na """
+        na_colname = "{}{}".format(prefix, colname)
+        cols_to_keep = list(self.data.columns) + [na_colname]
+        functions = ['mean', 'std', 'sem']
+        return self.data_isna_m.loc[:, cols_to_keep].groupby(na_colname).agg(functions).transpose()
+
+    def isna_summary(self, colname, prefix="is_na_"):
         """ Returns summary from one col with describe """
-        return df_isna.groupby(col).describe()
+        na_colname = "{}{}".format(prefix, colname)
+        cols_to_keep = list(self.data.columns) + [na_colname]
+        return self.data_isna_m.loc[:, cols_to_keep].groupby(na_colname).describe().transpose()
 
     def delete_narows(self, pct, index=False):
         """ Delete rows with more na percentage than > perc in data
